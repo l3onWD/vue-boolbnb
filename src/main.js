92f8731b226025c35f73065e8253281b730d6c27
l3on <l3onwebdev@gmail.com>
2023-11-16 18:35:28 +0100

    moved entry file and removed duplicated imports

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue';
+import App from "@/App.vue";
+
+// Bootstrap JS
+import 'bootstrap/js/index.esm';
+
+// Router
+import { router } from '@/router/';
+
+// Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Icons
+import { faMagnifyingGlass, faUser, faBars, faGlobe, faSliders, faFilter, faArrowDown, faChevronLeft, faLocationDot, faX, faThumbsUp, faQuestion, faAnglesUp } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
+library.add(faMagnifyingGlass, faUser, faBars, faGlobe, faSliders, faFacebook, faInstagram, faXTwitter, faFilter, faArrowDown, faChevronLeft, faLocationDot, faX, faThumbsUp, faQuestion, faAnglesUp)
+
+
+const app = createApp(App);
+
+app.use(router);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.mount("#app");
+
