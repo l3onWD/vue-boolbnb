9e5e8f78ab129a9e7a429410975c73afce8385c0
l3on <l3onwebdev@gmail.com>
2023-11-19 22:00:54 +0100

    fixed advanced filter modal logic

--- src/components/filters/FilterAdvanced.vue
+++ src/components/filters/FilterAdvanced.vue
@@ -1,150 +1,156 @@
-<script>
+<script setup>
+import { computed, reactive, ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 import { apiClient } from '@/http/';
-
 
 //*** COMPONENTS ***/
 import BaseModal from '@/components/base/BaseModal.vue';
 
 
-export default {
 
-    components: { BaseModal },
+//*** SERVICES ***/
+// Data
+const servicesList = reactive([]);
 
-    data: () => ({
-        servicesList: [],
-        filters: {
-            rooms: null,
-            beds: null,
-            radiusKm: 20,
-            'services[]': [],
-        },
-        isModalVisible: false,
-        totalFilters: 0
+// Functions
+const fetchServices = () => {
 
-    }),
-    computed: {
-        radius() {
-            return parseInt(this.filters.radiusKm) * 1000;
-        }
-    },
-    methods: {
-        fetchServices() {
+    apiClient.get('/services')
+        .then(({ data }) => {
+            servicesList.splice(-1);
+            servicesList.push(...data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
-            apiClient.get('/services')
-                .then(({ data }) => {
-                    this.servicesList = data;
-                })
-                .catch(err => {
-                    console.error(err);
-                });
-
-        },
-
-        resetFilters() {
-
-            // Reset Filters
-            this.filters = {
-                rooms: null,
-                beds: null,
-                radiusKm: 20,
-                'services[]': [],
-            }
-
-            // Call API
-            this.applyFilters();
-        },
-
-        applyFilters() {
-
-            // Create query params
-            const query = {
-                address: this.$route?.query.address,
-                lat: this.$route?.query.lat,
-                lon: this.$route?.query.lon,
-                category: this.$route?.query.category,
-                rooms: this.filters.rooms,
-                beds: this.filters.beds,
-                radius: this.radius,
-                'services[]': this.filters['services[]'],
-            }
-
-            // Apply query
-
-            this.$router.push({ name: 'search', query })
-            this.addFilters();
-        },
-
-
-        // Check if are any filters in the URL
-        assignFilters() {
-
-            if (this.$route?.query.rooms && this.$route?.query.rooms > 0) {
-                this.filters.rooms = this.$route?.query.rooms;
-            };
-
-            if (this.$route?.query.beds && this.$route?.query.beds > 0) {
-                this.filters.beds = this.$route?.query.beds;
-            };
-
-            if (this.$route?.query.radius && this.$route?.query.radius != 20000) {
-                this.filters.radiusKm = parseInt(this.$route?.query.radius) / 1000;
-            };
-
-            if (this.$route?.query['services[]'] && this.$route?.query['services[]'].length) {
-                this.filters['services[]'] = [...this.$route?.query['services[]']];
-            };
-
-        },
-
-        // Update Filters Counter
-        addFilters() {
-
-            this.totalFilters = 0;
-
-
-            // Count Filters
-            if (this.filters.rooms) {
-                this.totalFilters += 1;
-            };
-
-            if (this.filters.beds) {
-                this.totalFilters += 1;
-            }
-
-            if (this.filters.radiusKm != 20) {
-                this.totalFilters += 1;
-            }
-
-            this.totalFilters += this.filters['services[]'].length;
-        },
-
-    },
-
-    created() {
-        this.fetchServices();
-        this.assignFilters();
-        this.addFilters();
-
-    },
-
-    emits: ['@close']
 }
+
+// Get services
+fetchServices();
+
+
+
+//*** FILTERS MODAL ***/
+// Data
+const filters = reactive({
+    rooms: null,
+    beds: null,
+    radiusKm: 20,
+    'services[]': []
+});
+const isModalVisible = ref(false);
+const router = useRouter();
+const route = useRoute();
+
+// Compute Filters Count
+const filtersCount = computed(() => {
+
+    // Check if route is initialized
+    if (!route) return 0;
+
+    // Get data
+    const { rooms, beds, radius, 'services[]': services } = route.query;
+    let count = 0;
+
+    // Count query params
+    if (rooms) count++;
+    if (beds) count++;
+    if (radius != 20000) count++;
+    if (services) count += services.length;
+
+    return count;
+});
+
+// Compute Radius in meters (for API)
+const radius = computed(() => {
+    return parseInt(filters.radiusKm) * 1000;
+});
+
+
+// Functions
+const applyFilters = () => {
+
+    // Create query params
+    const query = {
+        address: route?.query.address,
+        lat: route?.query.lat,
+        lon: route?.query.lon,
+        category: route?.query.category,
+
+        rooms: filters.rooms,
+        beds: filters.beds,
+        radius: radius.value,
+        'services[]': filters['services[]'],
+    }
+
+    // Apply query
+    router.push({ name: 'search', query });
+
+    // Close modal
+    isModalVisible.value = false;
+}
+
+const resetFilters = () => {
+
+    // Reset filters to default
+    filters.rooms = null;
+    filters.beds = null;
+    filters.radiusKm = 20;
+    filters['services[]'] = [];
+
+    applyFilters();
+}
+
+const updateFilters = () => {
+
+    // Check if route is initialized
+    if (!route) return;
+
+    // Get data
+    const { rooms, beds, radius, 'services[]': services } = route.query;
+
+    // Reset filters to default
+    filters.rooms = null;
+    filters.beds = null;
+    filters.radiusKm = 20;
+    filters['services[]'] = [];
+
+    // Check query params
+    if (rooms) filters.rooms = rooms;
+    if (beds) filters.beds = beds;
+    if (radius && radius != 20000) filters.radiusKm = parseInt(radius) / 1000;
+    if (services && services.length) filters['services[]'] = [...services];
+}
+
+const onModalClose = () => {
+
+    // Reset unsetted filters
+    updateFilters();
+
+    // Close modal
+    isModalVisible.value = false;
+}
+
+// Update filters on create
+updateFilters();
+
 </script>
 
 
 <template>
     <!-- Advanced Filters Toggler -->
-    <button class="filter-advanced-toggler flex-shrink-0 ms-3" :class="{ 'has-filter': totalFilters }"
+    <button class="filter-advanced-toggler flex-shrink-0 ms-3" :class="{ 'has-filter': filtersCount }"
         @click="isModalVisible = !isModalVisible">
         <FontAwesomeIcon icon="sliders" />
         <span class="d-none d-md-inline">Filtri</span>
 
         <!-- Count Badge -->
-        <span v-if="totalFilters" class="filter-number">{{ totalFilters }}</span>
+        <span v-if="filtersCount" class="filter-number">{{ filtersCount }}</span>
     </button>
 
     <!-- Advanced Filters Modal -->
-    <BaseModal title="Filtri" :isVisible="isModalVisible" @@close="isModalVisible = false">
+    <BaseModal title="Filtri" :isVisible="isModalVisible" @@close="onModalClose">
 
         <!-- Filters -->
         <form @submit.prevent="applyFilters" class="container-fluid">
@@ -170,14 +176,18 @@ export default {
 
                 <!-- Services -->
                 <div class="col-12 mb-3">
+
                     <p class="fw-bold mb-2">Servizi</p>
+
                     <div class="row">
                         <div v-for="service in servicesList" :key="service.id" class="col-6 form-check">
 
+                            <!-- Checkbox -->
                             <input v-model.trim="filters['services[]']" class="form-check-input" type="checkbox"
                                 :id="service.id" :value="service.id">
 
-                            <label class="form-label d-flex align-items-center gap-1" :for="service.id">
+                            <!-- Service Label -->
+                            <label class="form-label d-flex align-items-center gap-2" :for="service.id">
                                 <div class="service-icon">
                                     <img :src="`http://127.0.0.1:8000/img/service/${service.icon}`" :alt="service.name">
                                 </div>
@@ -192,11 +202,12 @@ export default {
                 <div class="col-12 border-top text-end pt-3">
                     <button @click="resetFilters" type="button" class="btn btn-secondary">Cancella
                         Tutto</button>
-                    <button type="submit" class="btn btn-success ms-2" @click="isModalVisible = false">Mostra</button>
+                    <button type="submit" class="btn btn-success ms-2">Applica</button>
                 </div>
 
             </div>
         </form>
+
     </BaseModal>
 </template>
 
