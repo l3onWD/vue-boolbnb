db430366d9456be54096a3a779653f663fb8a311
l3on <l3onwebdev@gmail.com>
2023-11-19 13:51:55 +0100

    added filter categories component

---
+++ src/components/filters/FilterCategories.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+
+    props: {
+        categories: {
+            type: Array,
+            default: []
+        }
+    },
+
+    computed: {
+
+        queryParams() {
+            if (!this.$route) return null
+            return {
+                address: this.$route.query.address,
+                lat: this.$route.query.lat,
+                lon: this.$route.query.lon,
+                rooms: this.$route.query.rooms,
+                beds: this.$route.query.beds,
+                radius: this.$route.query.radius,
+                'services[]': this.$route.query['services[]']
+            }
+        }
+    },
+
+}
+</script>
+
+
+<template>
+    <ul class="filter-categories">
+        <li v-for="category in categories" :key="category.id">
+            <RouterLink :to="{ name: 'search', query: { ...queryParams, category: category.id } }"
+                class="filter-categories-link" :class="{ 'active': this.$route?.query.category === category.id }">
+                <img :src="`src/assets/img/category/${category.img}`" :alt="category.name">
+                <div>{{ category.name }}</div>
+            </RouterLink>
+        </li>
+    </ul>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+.filter-categories {
+    height: 80px;
+
+    @include flex(start);
+    gap: 15px;
+
+    overflow-x: scroll;
+
+    li {
+        height: 100%;
+    }
+
+    .filter-categories-link {
+        height: 100%;
+        padding: 0 10px;
+
+        @include flex($direction: column);
+        gap: 5px;
+        color: $col-grey;
+        font-size: 12px;
+        border-bottom: 3px solid transparent;
+        font-weight: 500;
+        text-wrap: nowrap;
+
+        transition: all 0.5s;
+
+        img {
+            width: 25px;
+
+            filter: invert(40%) sepia(55%) saturate(0%) hue-rotate(206deg) brightness(113%) contrast(83%);
+        }
+
+        &:hover,
+        &.active {
+            border-color: $col-grey;
+            color: black;
+
+            img {
+                filter: brightness(0) saturate(100%);
+            }
+        }
+    }
+}
+</style>
--- src/components/search/SearchFilters.vue
+++ src/components/search/SearchFilters.vue
@@ -2,8 +2,13 @@
 import { store } from '@/js/store.js';
 import { apiClient } from '@/http/';
 
+//*** COMPONENTS ***/
+import FilterCategories from '@/components/filters/FilterCategories.vue';
+
 
 export default {
+
+    components: { FilterCategories },
 
     data: () => ({
         categories: [],
@@ -11,21 +16,11 @@ export default {
     }),
 
     methods: {
-        fetchCategories() {
-            apiClient.get('/categories').then(res => { this.categories = res.data })
-        },
 
-        setQueryParams(category_id) {
-            return {
-                address: this.$route?.query.address,
-                lat: this.$route?.query.lat,
-                lon: this.$route?.query.lon,
-                category: category_id,
-                rooms: this.$route?.query.rooms,
-                beds: this.$route?.query.beds,
-                radius: this.$route?.query.radius,
-                'services[]': this.$route?.query['services[]'],
-            };
+        fetchCategories() {
+            apiClient.get('/categories')
+                .then(res => { this.categories = res.data })
+                .catch(err => { console.error(err) });
         }
     },
 
@@ -41,15 +36,7 @@ export default {
         <div class="container">
 
             <!-- Categories Filter -->
-            <ul class="filter-categories">
-                <li v-for="category in categories" :key="category.id">
-                    <RouterLink :to="{ name: 'search', query: setQueryParams(category.id) }" class="filter-categories-link"
-                        :class="{ 'active': $route.query?.category == category.id }">
-                        <img :src="`src/assets/img/category/${category.img}`" :alt="category.name">
-                        <div>{{ category.name }}</div>
-                    </RouterLink>
-                </li>
-            </ul>
+            <FilterCategories :categories="categories" />
 
             <!-- Advanced Filters Toggle -->
             <button class="filter-advanced-toggle flex-shrink-0 ms-3" data-bs-toggle="modal" data-bs-target="#searchModal"
@@ -75,50 +62,6 @@ export default {
     >.container {
         display: flex;
         align-items: center;
-    }
-
-    .filter-categories {
-        height: 80px;
-
-        @include flex(start);
-        gap: 15px;
-
-        overflow-x: scroll;
-
-        li {
-            height: 100%;
-        }
-
-        .filter-categories-link {
-            height: 100%;
-            padding: 0 10px;
-
-            @include flex($direction: column);
-            gap: 5px;
-            color: $col-grey;
-            font-size: 12px;
-            border-bottom: 3px solid transparent;
-            font-weight: 500;
-            text-wrap: nowrap;
-
-            transition: all 0.5s;
-
-            img {
-                width: 25px;
-
-                filter: invert(40%) sepia(55%) saturate(0%) hue-rotate(206deg) brightness(113%) contrast(83%);
-            }
-
-            &:hover,
-            &.active {
-                border-color: $col-grey;
-                color: black;
-
-                img {
-                    filter: brightness(0) saturate(100%);
-                }
-            }
-        }
     }
 
 
