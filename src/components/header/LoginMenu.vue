b248dd685b3d2294a158c619fe1c802f67530e0a
l3on <l3onwebdev@gmail.com>
2023-11-20 18:18:55 +0100

    fixed login menu html structure

--- src/components/header/LoginMenu.vue
+++ src/components/header/LoginMenu.vue
@@ -1,98 +1,117 @@
-<script>
+<script setup>
+import { ref } from 'vue';
 import { apiClient } from '@/http/';
 
 
-export default {
+//*** DATA ***/
+const user = ref('');
+const isLoading = ref(false);
 
-    data() {
-        return {
-            user: '',
-            loading: false
-        };
-    },
 
-    methods: {
-        // Get user details
-        fetchUser() {
-            this.loading = true;
+//*** FUNCTIONS ***/
+// Get user details
+const fetchUser = () => {
 
-            apiClient.get('/user')
-                .then(res => { this.user = res.data; this.loading = false })
-                .catch(err => { console.error(err.response.data) })
-                .then(() => { this.loading = false });
-        },
+    isLoading.value = true;
 
-        // Get First letter of a string
-        getFirstLetter: (word) => (word.substring(0, 1).toUpperCase()),
-    },
-
-    created() {
-        this.fetchUser();
-    }
-
+    apiClient.get('/user')
+        .then(res => { user.value = res.data; isLoading.value = false })
+        .catch(err => { console.error(err.response.data) })
+        .then(() => { isLoading.value = false });
 }
+
+// Get First letter of a string
+const getFirstLetter = (word) => (word.substring(0, 1).toUpperCase());
+
+
+//*** FETCH USERS ***/
+fetchUser();
+
 </script>
 
 
 <template>
     <div class="login-menu dropdown">
 
-        <!-- Generic Menu Togle -->
-        <button class="dropdown-toggle d-none d-md-flex align-items-center" data-bs-toggle="dropdown">
-            <FontAwesomeIcon icon="bars" />
+        <!-- Menu Toggle -->
+        <button class="dropdown-toggle d-flex align-items-center" data-bs-toggle="dropdown">
 
-            <div v-if="loading" class="user ms-2">
-                <div class="spinner-border" role="status" style="width: 15px; height: 15px;">
-                </div>
-            </div>
+            <!-- Bars icon -->
+            <FontAwesomeIcon icon="bars" class="d-none d-md-block me-2" />
 
-            <div v-else-if="user.length && !loading" class="user ms-2">
-                <span id="admin-name">{{ getFirstLetter(user[0]['name']) }}</span>
-            </div>
-
-            <div v-else class="user ms-2">
-                <FontAwesomeIcon icon="user" />
-            </div>
-
-        </button>
-
-        <!-- Logged Menu Toggle -->
-        <button class="dropdown-toggle d-flex d-md-none align-items-center" data-bs-toggle="dropdown">
             <div class="user">
-                <div v-if="loading" class="spinner-border" role="status" style="width: 15px; height: 15px;">
+
+                <!-- Bootstrap Spinner -->
+                <div v-if="isLoading" class="spinner-border" role="status" style="width: 15px; height: 15px;">
                 </div>
 
-                <span v-else-if="user.length && !loading" id="admin-name">{{
-                    getFirstLetter(user[0]['name'])
-                }}</span>
+                <!-- User Logged Letter -->
+                <span v-else-if="user.length && !isLoading" id="admin-name">{{ getFirstLetter(user[0]['name']) }}</span>
 
+                <!-- User Disconnected Icon -->
                 <FontAwesomeIcon v-else icon="user" />
+
             </div>
+
         </button>
 
-        <!-- Account Menu -->
-        <ul v-if="user.length" class="dropdown-menu">
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/admin/apartments">I miei
-                    Boolbnb</a>
-            </li>
-            <li><a class="dropdown-item " href="http://127.0.0.1:8000/admin">Il mio Profilo</a></li>
-            <li><a class="dropdown-item disabled" href="http://127.0.0.1:8000/login">Messaggi</a></li>
-            <hr>
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/admin/apartments/create">Apri un
-                    Boolbnb</a>
-            </li>
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/profile">Account</a></li>
-            <hr>
-            <li>
-                <RouterLink class="dropdown-item" :to="{ name: 'home' }">Torna alla Home</RouterLink>
-            </li>
-        </ul>
 
-        <!-- Login Menu -->
-        <ul v-else class="dropdown-menu">
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/login"><b>Accedi</b></a></li>
-            <li><a class="dropdown-item" href="http://127.0.0.1:8000/register">Registrati</a></li>
-            <hr>
+        <!-- Menu -->
+        <ul class="dropdown-menu">
+
+            <!-- Account Menu -->
+            <li v-if="user.length">
+                <ul>
+                    <li>
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/admin/apartments">I miei
+                            Boolbnb</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item " href="http://127.0.0.1:8000/admin">Il mio Profilo</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item disabled" href="http://127.0.0.1:8000/login">Messaggi</a>
+                    </li>
+
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/admin/apartments/create">Apri un
+                            Boolbnb</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/profile">Account</a>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Login Menu -->
+            <li v-else>
+                <ul>
+                    <li>
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/login"><b>Accedi</b></a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/register">Registrati</a>
+                    </li>
+
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <RouterLink class="dropdown-item" :to="{ name: 'home' }">Torna alla Home</RouterLink>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- General Links -->
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+
             <li>
                 <RouterLink class="dropdown-item" :to="{ name: 'home' }">Torna alla Home</RouterLink>
             </li>
@@ -103,7 +122,7 @@ export default {
 
 
 <style lang="scss" scoped>
-@use '../../assets/scss/vars' as *;
+@use '@/assets/scss/vars' as *;
 
 .login-menu {
     @include flex;
@@ -124,6 +143,7 @@ export default {
     }
 }
 
+// MEDIA MD
 @media (min-width: 768px) {
     .login-menu {
         padding: 0 6px 0 10px;
