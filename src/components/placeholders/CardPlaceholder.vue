b4928c3222aabbbbfb93ac7179b2099503f5d729
l3on <l3onwebdev@gmail.com>
2023-11-21 17:27:25 +0100

    added placeholder components

---
+++ src/components/placeholders/CardPlaceholder.vue
@@ -0,0 +1,59 @@
+<script></script>
+
+
+<template>
+    <div class="card" aria-hidden="true">
+
+        <div class="img-card-top card-img placeholder"></div>
+
+        <div class="card-body">
+
+            <h5 class="card-title placeholder-glow">
+                <span class="placeholder col-8"></span>
+            </h5>
+
+            <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+                <span class="placeholder col-6"></span>
+            </p>
+
+        </div>
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+.placeholder {
+    animation: fade 1s linear infinite alternate;
+    border-radius: 5px;
+}
+
+.card {
+    border: none;
+    border-radius: 0;
+}
+
+.card-img {
+    position: relative;
+    aspect-ratio: 1;
+    overflow: hidden;
+}
+
+.card-body {
+    padding: 16px 0;
+}
+
+@keyframes fade {
+    from {
+        background-color: $col-grey;
+    }
+
+    to {
+        background-color: $light-grey;
+    }
+}
+</style>
---
+++ src/components/placeholders/HeaderPlaceholder.vue
@@ -0,0 +1,33 @@
+<script></script>
+
+
+<template>
+    <div>
+        <h5 class="placeholder-glow mb-2">
+            <span class="placeholder col-4 col-md-3 col-lg-2"></span>
+        </h5>
+        <h6 class="placeholder-glow">
+            <span class="placeholder col-5 col-md-4 col-lg-3"></span>
+        </h6>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+.placeholder {
+    animation: fade 1s linear infinite alternate;
+    border-radius: 5px;
+}
+
+@keyframes fade {
+    from {
+        background-color: $col-grey;
+    }
+
+    to {
+        background-color: $light-grey;
+    }
+}
+</style>
