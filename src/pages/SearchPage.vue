fb0ce3cf60d123d9db5fd860638d7a19c4f47011
l3on <l3onwebdev@gmail.com>
2023-11-23 19:21:59 +0100

    added back top button on search page

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,26 +1,26 @@
 <script setup>
-import { computed, reactive, ref, watch } from 'vue';
+import { computed, onBeforeUnmount, onMounted, reactive, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { apiClient } from '@/http/';
 
 //*** COMPONENTS ***//
+import TheBackTopButton from '@/components/base/TheBackTopButton.vue';
 import FiltersBar from '@/components/filters/FiltersBar.vue';
 import ApartmentsList from '@/components/apartment/ApartmentsList.vue';
 
 
-//*** DATA ***//
+//*** SEARCH APARTMENTS ***//
+// Data
 const apartments = reactive([]);
 const address = ref('');
 const errorMessage = ref('');
 const isLoading = ref(true);
 const route = useRoute();
 
-
-//*** COMPUTED ***//
+// Computed
 const routeQuery = computed(() => route.query);
 
-
-//*** FUNCTIONS ***//
+// Functions
 const fetchApartments = () => {
 
     isLoading.value = true;
@@ -52,17 +52,62 @@ const fetchApartments = () => {
         });
 }
 
-
-//*** WATCHERS ***//
+// Watch query cahnges
 watch(routeQuery, () => {
     fetchApartments();
 }, { immediate: true });
+
+
+
+//*** BACK TOP BUTTON ***//
+// Data
+const backTopVisibilityThreshold = 200;
+const isBackTopVisible = ref(false);
+const mainContent = ref(null);
+const scrollThrottling = ref(false);
+
+// Functions
+const handleBackTopVisibility = () => {
+    if (mainContent.value.scrollTop >= backTopVisibilityThreshold) isBackTopVisible.value = true;
+    else isBackTopVisible.value = false;
+}
+
+const backTop = () => {
+    mainContent.value.scrollTop = 0;
+}
+
+const handleScroll = () => {
+
+    // Scroll Throttling
+    if (!scrollThrottling.value) {
+
+        window.requestAnimationFrame(() => {
+
+            // Back top visibility logic
+            handleBackTopVisibility();
+
+            scrollThrottling.value = false;
+        });
+
+        scrollThrottling.value = true;
+    }
+
+}
+
+// Lifehooks
+onMounted(() => {
+    mainContent.value.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+    mainContent.value.removeEventListener('scroll', handleScroll);
+});
 
 </script>
 
 
 <template>
-    <main>
+    <main ref="mainContent">
 
         <!-- Filters -->
         <div class="filters-bar-container">
@@ -81,6 +126,11 @@ watch(routeQuery, () => {
             <ApartmentsList :apartments="apartments" :title="`Risultati in questa località: ${address}`"
                 infoMessage="Vengono visualizzati per primi i boolbnb consigliati dal nostro team" :isLoading="isLoading" />
         </div>
+
+
+        <!-- Back-top-button -->
+        <TheBackTopButton :isVisible="isBackTopVisible" @@back-top="backTop" />
+
     </main>
 </template>
 
