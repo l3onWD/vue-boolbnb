4a215e3991e0c11c7b642cae8df8c22b12e953e1
l3on <l3onwebdev@gmail.com>
2023-11-24 17:59:34 +0100

    added user store

---
+++ src/stores/UserStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+
+export const useUserStore = defineStore('user', () => {
+
+    //*** DATA ***//
+    const user = ref(null);
+
+    //*** GETTERS ***//
+    const isLogged = computed(() => user.value !== null);
+
+    //*** ACTIONS ***//
+    const setUser = user => { user.value = user };
+    const removeUser = user => { user.value = null };
+
+
+    return { user, isLogged, setUser, removeUser };
+
+});
