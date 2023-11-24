ea353ab009db7e2b931910edb3a58914258de621
l3on <l3onwebdev@gmail.com>
2023-11-24 17:30:54 +0100

    added dashboard test page

---
+++ src/pages/admin/DashboardPage.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div id="dashboard-page" class="container mb-4">
+
+        <header class="py-4">
+            <h2>I tuoi boolbnb</h2>
+        </header>
+
+        <table class="table table-white table-hover align-middle">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titolo</th>
+                    <th>Stato</th>
+                    <th>Categoria</th>
+                    <th>Fine Promozione</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>test</td>
+                    <td>pubblicato</td>
+                    <td>Wow!</td>
+                    <td>-</td>
+                    <td>
+                        <button class="button">Test</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+
+<style scoped></style>
