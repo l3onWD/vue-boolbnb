10b10d9c441d7395eb601532d995a75a88db704c
l3on <l3onwebdev@gmail.com>
2023-11-24 17:59:23 +0100

    imported pinia into main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from 'vue';
-import App from "@/App.vue";
-
+import { createPinia } from 'pinia'
 // Bootstrap JS
 import 'bootstrap/js/index.esm';
-
 // Router
 import { router } from '@/router/';
-
 // Custom Directives
 import { clickOutside } from '@/directives/';
-
 // Fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -20,9 +16,16 @@ import { faFacebook, faInstagram, faXTwitter } from '@fortawesome/free-brands-sv
 library.add(faMagnifyingGlass, faUser, faBars, faGlobe, faSliders, faFacebook, faInstagram, faXTwitter, faFilter, faArrowDown, faChevronLeft, faLocationDot, faX, faThumbsUp, faQuestion, faAnglesUp)
 
 
+//*** COMPONENTS ***//
+import App from "@/App.vue";
+
+
+//*** INIT ***//
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.directive('click-outside', clickOutside);
 
