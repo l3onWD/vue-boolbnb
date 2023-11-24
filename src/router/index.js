927660d452b2fcc06211ebfbe90759bab2ebed3d
l3on <l3onwebdev@gmail.com>
2023-11-24 17:31:39 +0100

    added login and dashboard routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
 import ApartmentPage from '@/pages/ApartmentPage.vue';
 import SearchPage from '@/pages/SearchPage.vue';
+import DashboardPage from '@/pages/admin/DashboardPage.vue';
+import LoginPage from '@/pages/auth/LoginPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 
 const router = createRouter({
@@ -10,6 +12,10 @@ const router = createRouter({
         { path: '/', name: 'home', component: HomePage },
         { path: '/apartments/:id', name: 'apartment', component: ApartmentPage },
         { path: '/search', name: 'search', component: SearchPage },
+
+        { path: '/dashboard', name: 'dashboard', component: DashboardPage },
+
+        { path: '/login', name: 'login', component: LoginPage },
 
         { path: '/not-found', name: 'not-found', component: NotFoundPage },
         { path: '/:pathMatch(.*)*', redirect: '/not-found' },
